04862e1aa42ea476acf29d865a40591c07a79e25
Serroria <139000554+Serroria@users.noreply.github.com>
2024-09-07 12:42:23 +0700

    Add files via upload

---
+++ bingo/Bingo.java
@@ -0,0 +1,76 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package test.bingo;
+
+/**
+ *
+ * @author meryl
+ */
+public class Bingo {
+String kalimat;
+    public Bingo(){
+        this.kalimat=kalimat;
+    }
+public void bagian1(){
+System.out.println("There was a farmer who had a dog,");
+System.out.println("And Bingo was his name-o.");
+System.out.println("B-I-N-G-O");
+System.out.println("B-I-N-G-O");
+System.out.println("B-I-N-G-O");
+System.out.println("And Bingo was his name-o.");
+System.out.println();
+}
+public void bagian2(){
+    System.out.println("There was a farmer who had a dog,");
+    System.out.println("And Bingo was his name-o.");
+    System.out.println("(clap)-I-N-G-O");
+    System.out.println("(clap)-I-N-G-O");
+    System.out.println("(clap)-I-N-G-O");
+    System.out.println("And Bingo was his name-o.");
+    System.out.println();
+}
+
+public void bagian3(){
+    System.out.println("There was a farmer who had a dog,");
+    System.out.println("And Bingo was his name-o.");
+    System.out.println("(clap)-(clap)-N-G-O");
+    System.out.println("(clap)-(clap)-N-G-O");
+    System.out.println("(clap)-(clap)-N-G-O");
+    System.out.println("(clap)-(clap)-N-G-O");
+    System.out.println("And Bingo was his name-o.");
+    System.out.println();
+}
+public void bagian4(){
+    System.out.println("There was a farmer who had a dog,");
+    System.out.println("And Bingo was his name-o.");
+    System.out.println("(clap)-(clap)-(clap)-G-O");
+    System.out.println("(clap)-(clap)-(clap)-G-O");
+    System.out.println("(clap)-(clap)-(clap)-G-O");
+    System.out.println("And Bingo was his name-o.");
+    System.out.println();
+}
+public void bagian5(){
+    System.out.println("There was a farmer who had a dog,");
+    System.out.println("And Bingo was his name-o.");
+    System.out.println("(clap)-(clap)-(clap)-(clap)-O");
+    System.out.println("(clap)-(clap)-(clap)-(clap)-O");
+    System.out.println("(clap)-(clap)-(clap)-(clap)-O");
+    System.out.println("And Bingo was his name-o.");
+    System.out.println();
+}
+public void bagian6(){
+    System.out.println("There was a farmer who had a dog,");
+    System.out.println("And Bingo was his name-o.");
+    System.out.println("(clap)-(clap)-(clap)-(clap)-(clap)");
+    System.out.println("(clap)-(clap)-(clap)-(clap)-(clap)");
+    System.out.println("(clap)-(clap)-(clap)-(clap)-(clap)");
+    System.out.println("And Bingo was his name-o.");
+    System.out.println();
+    
+    
+}
+
+}
+
---
+++ bingo/BingoBeraksi.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package test.bingo;
+
+/**
+ *
+ * @author meryl
+ */
+public class BingoBeraksi {
+    public static void main(String[]args){
+    Bingo bi = new Bingo();
+        bi.bagian1();
+        bi.bagian2();
+        bi.bagian3();
+        bi.bagian4();
+        bi.bagian5();
+        bi.bagian6();}
+}
